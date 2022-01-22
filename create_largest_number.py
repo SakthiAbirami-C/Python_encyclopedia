ec4194f6c6980517d1be867b849b328cc74df3ea
SakthiAbirami-C <98205103+SakthiAbirami-C@users.noreply.github.com>
2022-01-22 12:41:32 +0530

    Create create_largest_number.py
    Write a python function, create_largest_number(), which accepts a list of numbers and returns the largest number possible by concatenating the list of numbers. 
    Note: Assume that all the numbers are two digit numbers.
     
    
    Sample Input    Expected Output
    
    23,34,55             553423

---
+++ create_largest_number.py
@@ -0,0 +1,13 @@
+def create_largest_number(number_list):
+    number_list.sort(reverse=True)
+    largest_number = str(number_list[0])
+    for i in range(0,len(number_list)-1):
+        largest_number += str(number_list[i+1])
+    return int(largest_number)
+        
+    
+
+
+number_list=[23,45,67]
+largest_number=create_largest_number(number_list)
+print(largest_number)
